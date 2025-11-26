0afdc1847ae66a3d0eabf8a846ab0460838054ed
NanBohdanMoroz <92790530+NanSigmaBohdanMoroz@users.noreply.github.com>
2025-11-26 21:08:43 +0200

    delete test code

--- DocuSign.Workspaces/DocuSign.Workspaces/Controllers/WealthManagement/WealthManagementClientController.cs
+++ DocuSign.Workspaces/DocuSign.Workspaces/Controllers/WealthManagement/WealthManagementClientController.cs
@@ -21,24 +21,6 @@ public class WealthManagementClientController(IWealthManagementClient wealthMana
     [Route("/api/workspaces/add-selected-documents")]
     public async Task<List<EnvelopeModel>> AddSelectedDocuments([FromBody] WorkspaceAddDocumentsModel model)
     {
-        // var workspaceId = await wealthManager.CreateWorkspaces(new CreateWorkspacesModel()
-        // {
-        //     AccountId = "8a1318df-e2d3-48af-ae26-27fa85fe80e9",
-        //     OwnerEmail = "test@mail.com",
-        //     WorkspacesName = "test_workspace"
-        // });
-        var bytes = await System.IO.File.ReadAllBytesAsync("./Docs/World_Wide_Corp_lorem.pdf");
-        var documents = new List<string>
-        {
-            Convert.ToBase64String(bytes)
-        };
-
-        model.Documents = documents;
-        //
-        // var model = new WorkspaceAddDocumentsModel(
-        //     "4b6e3378-2fda-498b-86e7-55513966f05a",
-        //     "workspaceId",
-        //     documents);
         var envelopes = await wealthManagementClient.AddSelectedDocumentsForClientPackage(model);
         return envelopes;
     }
