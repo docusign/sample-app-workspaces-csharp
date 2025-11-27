b748842b37e8a8f3ea39c1a7655bcc875b54100a
NanSigmaBohdanMoroz <92790530+NanSigmaBohdanMoroz@users.noreply.github.com>
2025-11-27 13:57:18 +0200

    Backend enpoints for Case1 (#7)

--- DocuSign.Workspaces/DocuSign.Workspaces/Domain/Workspaces/Models/CreateWorkspacesModel.cs
+++ DocuSign.Workspaces/DocuSign.Workspaces/Domain/Workspaces/Models/CreateWorkspacesModel.cs
@@ -3,8 +3,4 @@ namespace DocuSign.Workspaces.Domain.Workspaces.Models;
 public class CreateWorkspacesModel
 {
     public string WorkspacesName { get; set; }
-
-    public string OwnerEmail { get; set; }
-
-    public string AccountId { get; set; }
 }
