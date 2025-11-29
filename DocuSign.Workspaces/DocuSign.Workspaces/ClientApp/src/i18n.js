369e205ca3497621d7abf3f55f9b150d674a2b01
Andrii Yevarnytskyi <andrii.yevarnytskyi@sigma.software>
2025-11-29 15:11:04 +0100

    feat: other language was removed

--- DocuSign.Workspaces/DocuSign.Workspaces/ClientApp/src/i18n.js
+++ DocuSign.Workspaces/DocuSign.Workspaces/ClientApp/src/i18n.js
@@ -2,7 +2,7 @@ import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
 import enTranslation from './locales/en/translation.json';
-import ukTranslation from './locales/uk/translation.json';
+
 
 i18n
   .use(LanguageDetector)
@@ -17,9 +17,7 @@ i18n
       en: {
         translation: enTranslation,
       },
-      uk: {
-        translation: ukTranslation,
-      },
+
     },
   });
 
