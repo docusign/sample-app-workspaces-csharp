e54e13a541bc63c6b312b8d3673436a8dea34b31
Andrii Yevarnytskyi <andrii.yevarnytskyi@sigma.software>
2025-12-02 18:38:33 +0100

    feat: StepProgress adapter for small screen

--- DocuSign.Workspaces/DocuSign.Workspaces/ClientApp/src/components/StepProgress.js
+++ DocuSign.Workspaces/DocuSign.Workspaces/ClientApp/src/components/StepProgress.js
@@ -1,6 +1,48 @@
+import React, { useState, useEffect } from 'react';
 import { ReactComponent as CheckIcon } from '../assets/icons/check.svg';
 
 const StepProgress = ({ steps, currentStep }) => {
+  const [isCompact, setIsCompact] = useState(window.innerWidth < 420);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setIsCompact(window.innerWidth < 420);
+    };
+
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
+  if (isCompact) {
+    return (
+      <div className="step-progress-compact">
+        <div className="step-text">
+          STEP {currentStep + 1} OF {steps.length}
+        </div>
+        <div className="step-dots">
+          {steps.map((step, index) => {
+            const isCompleted = index < currentStep;
+            const isActive = index === currentStep;
+
+            return (
+              <div
+                key={index}
+                className={`step-dot step-circle ${isCompleted ? 'completed' : isActive ? 'active' : ''}`}
+              >
+                {isCompleted ? (
+                  <CheckIcon className="check_icon" />
+                ) : isActive ? (
+                  <div className={'step-circle-active '} />
+                ) : (
+                  ''
+                )}
+              </div>
+            );
+          })}
+        </div>
+      </div>
+    );
+  }
   return (
     <div className="step-progress">
       {steps.map((step, index) => {
